9f70c1d38af40efb7cef8c5cc3afc7ede07059d4
hjoyheath <hillaryjoyheath@gmail.com>
2022-04-06 19:53:51 -0400

    hyperlinked repos and rounded photo

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 console.log("linked");
 
 let profileDiv = document.querySelector("#profile");
+let repo = document.querySelector("#repo");
 
 fetch("https://api.github.com/users/hjoyheath", {
   method: "GET",
@@ -9,27 +10,43 @@ fetch("https://api.github.com/users/hjoyheath", {
   .then(function (response) {
     return response.json();
   })
+  //   avatar
   .then(function (data) {
     let imageEl = document.createElement("img");
     imageEl.src = data.avatar_url;
     profileDiv.appendChild(imageEl);
-
+    // name
     let nameDiv = document.createElement("p");
     nameDiv.classList.add("name");
     nameDiv.innerText = data.name;
     profileDiv.appendChild(nameDiv);
-
+    // location
     let locationDiv = document.createElement("div");
     locationDiv.innerText = `Location: ${data.location}`;
     profileDiv.appendChild(locationDiv);
-
+    // gitHub url
     let gitDiv = document.createElement("div");
-    gitDiv.innerText = `Git Hub URL: ${data.html_url}`;
+    gitDiv.innerText = '<a href ="' + "GitHub URL:" + data.html_url + "</a>";
     profileDiv.appendChild(gitDiv);
-
+    // username
     let usernameDiv = document.createElement("div");
-    usernameDiv.innerText = `Git Hub Username: ${data.login}`;
+    usernameDiv.innerText = `GitHub Username: ${data.login}`;
     profileDiv.appendChild(usernameDiv);
   });
+
 //   urlto get all my repos
-// "https://api.github.com/users/hjoyheath/repos"
+fetch("https://api.github.com/users/hjoyheath/repos", {
+  method: "GET",
+  headers: {},
+})
+  .then(function (response) {
+    return response.json();
+  })
+
+  .then(function (data) {
+    for (let i of data) {
+      let repoDiv = document.createElement("div");
+      repoDiv.innerHTML = '<a href="' + i.html_url + '">' + i.name + "</a>";
+      profileDiv.appendChild(repoDiv);
+    }
+  });
